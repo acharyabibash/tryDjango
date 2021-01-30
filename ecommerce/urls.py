cd2f32cb977aad66f92f214b52d1a6061dba10d4
acharyabibash <acharabibash@outlook.com>
2021-01-30 13:26:11 +0545

    18 Try Django 1.11

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -13,10 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
+
+from django.conf.urls.static import static
+
+
 from django.contrib import admin
 from django.urls import path
 
 from .views import home_page,about_page,contact_page,login_page,register_page
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +34,7 @@ urlpatterns = [
     path('login/', login_page),
     path('register/', login_page),
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
